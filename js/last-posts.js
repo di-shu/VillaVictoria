1c223f834feac0d075adcfb9374dd81363d5cf52
di-shu <54156812+di-shu@users.noreply.github.com>
2020-06-09 17:37:31 +0300

    bug fixing

--- js/last-posts.js
+++ js/last-posts.js
@@ -1,21 +1,25 @@
-$.ajax({
-    url: "https://www.instagram.com/"+'villa.victoria.od'+"/?__a=1",
-    success(data) {
-      const edges = data.graphql.user.edge_owner_to_timeline_media.edges
-      edges.forEach((item, i) => {
-        let itemSrc = item.node.display_url
-        if(i<3) {
-            $('#instafeed').append(
-                '<a href="https://www.instagram.com/p/' +
-                item.node.shortcode +
-                '/" class="instafeed__box" target="_blank" id="' +
-                i +
-                '"><img class="instafeed__img" src="' +
-                itemSrc +
-                '" /></a>'
-              )
+
+export default function getPosts() {
+    $.ajax({
+        url: "https://www.instagram.com/"+'villa.victoria.od'+"/?__a=1",
+        success(data) {
+          const edges = data.graphql.user.edge_owner_to_timeline_media.edges
+          edges.forEach((item, i) => {
+            let itemSrc = item.node.display_url
+            if(i<3) {
+                $('#instafeed').append(
+                    '<a href="https://www.instagram.com/p/' +
+                    item.node.shortcode +
+                    '/" class="instafeed__box" target="_blank" id="' +
+                    i +
+                    '"><img class="instafeed__img" src="' +
+                    itemSrc +
+                    '" /></a>'
+                  )
+            }
+           
+          })
         }
-       
       })
-    }
-  })
+}
+
